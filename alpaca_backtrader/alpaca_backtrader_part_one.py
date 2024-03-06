8da7d98c6d844f83216e7537515701c8f13b4b10
fuwentay <fuwentay@gmail.com>
2024-03-06 19:12:39 +0000

    minor changes

--- alpaca_backtrader/alpaca_backtrader_part_one.py
+++ alpaca_backtrader/alpaca_backtrader_part_one.py
@@ -2,7 +2,7 @@ import alpaca_backtrader_api as alpaca
 import backtrader as bt
 import pytz
 from datetime import datetime
-from local_settings import alpaca_paper
+from alpaca_backtrader.local_settings import alpaca_paper
 
 ALPACA_KEY_ID = alpaca_paper['api_key']
 ALPACA_SECRET_KEY = alpaca_paper['api_secret']
--- alpaca_backtrader/local_settings.py
+++
@@ -1,4 +0,0 @@
-alpaca_paper = {
-    'api_key': 'PK82HVJDQBJQ08OP1L49',
-    'api_secret': 'JijIw6RMeILVdmRBL9NcVgPM973daA58ydV8oeYA',
-}
